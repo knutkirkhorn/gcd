532aa07b78b049ca0504cc2402451affe67b88dc
Knut Kirkhorn <knutkirk@hotmail.com>
2019-07-04 19:34:15 +0200

    Minor tweaks

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 import test from 'ava';
-import m from '.';
+import gcd from '.';
 
 test('gcd for some numbers', t => {
-    t.is(m(416, 86), 2);
-    t.is(m(51, 91), 1);
-    t.is(m(25, 5), 5);
+    t.is(gcd(416, 86), 2);
+    t.is(gcd(51, 91), 1);
+    t.is(gcd(25, 5), 5);
 });
