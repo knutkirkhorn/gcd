8d0b1c7c96bb80ca78ede88f84be718cde75c0d5
Knut Kirkhorn <knutkirk@hotmail.com>
2023-02-19 19:44:16 +0100

    Use tab indentation

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 export default function gcd(number1, number2) {
-    if (number2 === 0) {
-        return number1;
-    }
+	if (number2 === 0) return number1;
 
-    return gcd(number2, number1 % number2);
+	return gcd(number2, number1 % number2);
 }
--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import test from 'ava';
 import gcd from './index.js';
 
 test('gcd for some numbers', t => {
-    t.is(gcd(416, 86), 2);
-    t.is(gcd(51, 91), 1);
-    t.is(gcd(25, 5), 5);
+	t.is(gcd(416, 86), 2);
+	t.is(gcd(51, 91), 1);
+	t.is(gcd(25, 5), 5);
 });
